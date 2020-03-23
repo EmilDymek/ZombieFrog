0f16fe36ed33afd3f03612d6b38409f064317cdc
Emil Dymek <emil.dymek@gmail.com>
2020-03-23 17:10:36 +0100

    Basic Movement
    Sample texture used for Player Character

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public float PlayerSpeed;                                                   //Keeps Player Speed
+    private Vector2 PlayerDirection;                                            //Keeps track of PlayerDirection
+    private Rigidbody2D rb2d;                                                   //Keeps a reference to rigidbody
+
+    void Start()                                                                //This is the Start function
+    {
+        rb2d = GetComponent<Rigidbody2D>();                                     //Initializes the rigidbody
+    }
+
+
+    void Update()                                                               //This is the update function
+    {
+        GetInput();                                                             //Calls the GetInput Function
+    }
+
+
+    void GetInput()                                                             //This function checks for player input and moves the direction of movement accordingly
+    {
+        PlayerDirection = Vector2.zero;                                         //Zeroes the cameras direction
+        if (Input.GetKey(KeyCode.W))                                            //Checks if W is being pressed
+        {
+            PlayerDirection += Vector2.up;                                      //Sets direction to up if W is being pressed
+        }
+        if (Input.GetKey(KeyCode.A))                                            //Checks if A is being pressed
+        {
+            PlayerDirection += Vector2.left;                                    //Sets direction to left if A is being pressed
+        }
+        if (Input.GetKey(KeyCode.S))                                            //Checks if S is being pressed
+        {
+            PlayerDirection += Vector2.down;                                    //Sets direction to down if S is being pressed
+        }
+        if (Input.GetKey(KeyCode.D))                                            //Checks if D is being pressed
+        {
+            PlayerDirection += Vector2.right;                                   //Sets direction to right if D is being pressed
+        }
+
+        transform.Translate(PlayerDirection * PlayerSpeed * Time.deltaTime);    //Moves the player according to the set direction
+    }
+}
