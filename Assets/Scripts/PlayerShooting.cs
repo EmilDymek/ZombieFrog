b43915229439ae305f555f54b116f8e256b394f2
Emil Dymek <emil.dymek@gmail.com>
2020-04-09 14:51:25 +0200

    Reload

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -4,23 +4,34 @@ using UnityEngine;
 
 public class PlayerShooting : MonoBehaviour
 {
-    public float FireTimer = 0.1f;                                           //Variable that sotres fire rate
+    public float FireTimer = 0.01f;                                          //Variable that sotres fire rate
+    public float ReloadDelay = 1.0f;                                        //Variable that stores the time to reload
     public float GunDamage = 1.0f;                                          //Variable that stores bullet damage
     Vector3 MousePos;                                                       //Variable to store mouse position
     Vector3 GunPos;                                                         //Variable to store gun direction
     public float Angle;                                                     //Variable to store the rotation
+    public float PlayerBullets;                                             //Variable that stores the current ammount of bullets in the player's gun
+    public float MagazineSize;                                              //Variable that stores the fixed Magazine Size
     public Transform Target;                                                //Gameobject reference (Store the crosshair gameobject in here)
     public Transform FirePoint01;                                           //GameObject reference (Store FirePoint01 gameobject in here)
     public GameObject Bullet01;                                             //GameObject reference (Store Bullet Prefab in here)
     public GameObject ImpactEffect;                                         //Gameobject Reference (Store Impact Effect here)
     public LineRenderer LineRend;                                           //Reference to a linerenderer
-    bool Shooting = false;                                                  //Bool to keep track of wether or not the player is shooting
+    public AmmoTracking ammoTracking;
+
+    private void Awake()
+    {
+        ammoTracking.GetComponent<AmmoTracking>();
+    }
 
     void Update()
     {
         TurnGun();                                                          //Calls Turning function
         GetInput();                                                         //Calls the Input function
-        FireTimer -= Time.deltaTime;                                        //Ticks down Fire Timer (Time between shots)
+        if (FireTimer > 0)
+        {
+            FireTimer -= Time.deltaTime;                                        //Ticks down Fire Timer (Time between shots)
+        }
     }
 
     IEnumerator Shoot()
@@ -44,25 +55,36 @@ public class PlayerShooting : MonoBehaviour
             LineRend.SetPosition(0, FirePoint01.position);                                      //Sets first point of the line on the fire point position
             LineRend.SetPosition(1, FirePoint01.position + FirePoint01.right * 100);            //Sets the second point of the line 100 units away from the firepoint in the direction of the gun
         }
-
+        PlayerBullets -= 1;
         LineRend.enabled = true;                                                                //Draw the line
         yield return new WaitForSeconds(0.02f);                                                 //Wait for 0.02 seconds
         LineRend.enabled = false;                                                               //Hide the line
     }
 
+    IEnumerator Reload()
+    {
+        yield return new WaitForSeconds(ReloadDelay);
+        PlayerBullets = MagazineSize;
+
+    }
+
     void GetInput()
     {
-        if (Input.GetKey(KeyCode.Mouse0))                                                       //If the left mouse button is pressed
+        if (Input.GetKey(KeyCode.Mouse0) && FireTimer <= 0 && PlayerBullets > 0 && ammoTracking.Reloading == false)                //If the left mouse button is pressed AND FireTimer is at 0 AND the player has more than 0 bullets
         {
-            Shooting = true;                                                                    //Set Shooting bool to true
-        } else
-        {
-            Shooting = false;                                                                   //Else set shooting bool to false
-        }
-        if (Shooting == true && FireTimer <= 0) {                                               //If the shooting condition is true
             FireTimer = 0.1f;                                                                   //Reset Firetimer
             StartCoroutine(Shoot());                                                            //Calls Shooting coroutine
+        } 
+        if (Input.GetKey(KeyCode.R))
+        {
+            if (ammoTracking.Reloading == false && PlayerBullets != MagazineSize)
+            {
+                ammoTracking.Reloading = true;
+                ammoTracking.DisplayAmmount = 0;
+                StartCoroutine(Reload());
+            }
         }
+       
     }
 
     void TurnGun()
