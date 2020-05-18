ab40a9407715a3e4ab43fd63f441c6610721de1e
Emil Dymek <emil.dymek@gmail.com>
2020-05-18 16:42:53 +0200

    Buildscene Astar Setup

--- Assets/Scripts/StunQuake.cs
+++ Assets/Scripts/StunQuake.cs
@@ -2,39 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StunQuake : MonoBehaviour
+namespace Pathfinding
 {
-    public GameMaster GM;
-    public GameObject GMobj;
-
-    private float QuakeTimer;
-    private CircleCollider2D QuakeCollider;
-
-    void Awake()
+    public class StunQuake : VersionedMonoBehaviour
     {
-        GMobj = GameObject.Find("GM");
-        GM = GMobj.GetComponent<GameMaster>();
-        QuakeCollider = GetComponent<CircleCollider2D>();
-        QuakeTimer = GM.QuakeDuration;
-    }
+        public GameMaster GM;
+        public GameObject GMobj;
 
-    void Update()
-    {
-        transform.localScale += Vector3.one * GM.QuakeGrowth * Time.deltaTime;
-        QuakeTimer -= Time.deltaTime;
+        private float QuakeTimer;
+        private CircleCollider2D QuakeCollider;
 
-        if (QuakeTimer <= 0)
+        void Awake()
         {
-            Destroy(gameObject);
+            GMobj = GameObject.Find("GM");
+            GM = GMobj.GetComponent<GameMaster>();
+            QuakeCollider = GetComponent<CircleCollider2D>();
+            QuakeTimer = GM.QuakeDuration;
         }
-    }
 
-    private void OnTriggerEnter2D(Collider2D HitInfo)
-    {
-        if (HitInfo.tag == "Enemy")
+        void Update()
         {
-            GruntBehaviour Enemy = HitInfo.GetComponent<GruntBehaviour>();
-            Enemy.TakeStun();
+            transform.localScale += Vector3.one * GM.QuakeGrowth * Time.deltaTime;
+            QuakeTimer -= Time.deltaTime;
+
+            if (QuakeTimer <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D HitInfo)
+        {
+            if (HitInfo.tag == "Enemy")
+            {
+                AstarEnemyHitbox Enemy = HitInfo.GetComponent<AstarEnemyHitbox>();
+                Enemy.TakeStun();
+            }
         }
     }
 }
