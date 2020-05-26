268f7889012c511b21108980d574514ff455fea8
Emil <Dymek>
2020-05-26 17:32:58 +0200

    Sound Mixer Architecture

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -28,6 +28,8 @@ public class AudioManager : MonoBehaviour
             s.source = gameObject.AddComponent<AudioSource>();
             s.source.clip = s.clip;
 
+            s.source.outputAudioMixerGroup = s.audioMixerGroup;
+
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
             s.source.loop = s.loop;
